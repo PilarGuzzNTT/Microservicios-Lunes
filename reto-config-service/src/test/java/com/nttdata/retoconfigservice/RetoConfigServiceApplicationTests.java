13ff6f190e1dd5bce114d7227bb7ef02cfbf4f88
PilarGuzzNTT <pguzmanc@emeal.nttdata.com>
2023-05-08 14:16:23 +0200

    subida reto 1 y reto 2

---
+++ reto-config-service/src/test/java/com/nttdata/retoconfigservice/RetoConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.retoconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
